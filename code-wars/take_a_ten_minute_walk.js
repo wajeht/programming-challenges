927ced5da552677e2ccee38bb48a8f335fd7b457
wajeht <zombyard@gmail.com>
2021-02-01 22:48:58 -0600

    Update take_a_ten_minute_walk.js

--- code-wars/take_a_ten_minute_walk.js
+++ code-wars/take_a_ten_minute_walk.js
@@ -34,3 +34,30 @@ function isValidWalk(walk) {
 
     return walk.length == 10 && ns === 0 && we === 0;
 }
+
+// ----------------------------------------------------------------------------
+// best practice
+function isValidWalk(walk) {
+    var dx = 0;
+    var dy = 0;
+    var dt = walk.length;
+
+    for (var i = 0; i < walk.length; i++) {
+        switch (walk[i]) {
+            case 'n':
+                dy--;
+                break;
+            case 's':
+                dy++;
+                break;
+            case 'w':
+                dx--;
+                break;
+            case 'e':
+                dx++;
+                break;
+        }
+    }
+
+    return dt === 10 && dx === 0 && dy === 0;
+}
