2d47fd75f1b2ccae12d6878739cfc61b49fd9086
wajeht <zombyard@gmail.com>
2021-02-04 14:27:23 -0600

    Add regular_ball_super_balls.js

---
+++ code-wars/regular_ball_super_ball.js
@@ -0,0 +1,28 @@
+// https://www.codewars.com/kata/53f0f358b9cb376eca001079/train/javascript
+// my implementation
+
+class Ball {
+    constructor(balltype) {
+        if (balltype) {
+            this.balltype = balltype;
+        } else {
+            this.balltype = 'regular';
+        }
+    }
+}
+
+const b = new Ball();
+console.log(b.balltype);
+
+// best practice
+// -------------------------------------------------------------------------
+class Ball {
+    constructor(ballType = 'regular') {
+        this.ballType = ballType;
+    }
+} // best practice
+
+// -------------------------------------------------------------------------
+var Ball = function (ballType) {
+    this.ballType = ballType || 'regular';
+};
