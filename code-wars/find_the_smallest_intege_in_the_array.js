65e14047729f75f086b18076518082dfad08cd5a
wajeht <zombyard@gmail.com>
2021-02-10 21:22:30 -0600

    Add find_the_smallest_int_in_the_array.js

--- code-wars/find_the_smallest_intege_in_the_array.js
+++ code-wars/find_the_smallest_intege_in_the_array.js
@@ -1,11 +1,12 @@
 // https://www.codewars.com/kata/55a2d7ebe362935a210000b2/train/javascript
 // my implementation
-
+// i tried to to use the array.sort() function
 class SmallestIntegerFinder {
     findSmallestInt(args) {
+        let smallestNumber = Number.POSITIVE_INFINITY;
+
         for (let currentElement of args) {
-            let smallestNumber = 0;
-            if (smallestNumber < currentElement) {
+            if (smallestNumber > currentElement) {
                 smallestNumber = currentElement;
             }
         }
@@ -18,3 +19,11 @@ class SmallestIntegerFinder {
 
 const sif = new SmallestIntegerFinder();
 sif.findSmallestInt([1, 56, 232, 12, 8]);
+
+// clever solution
+// ----------------------------------------------------------------------
+class SmallestIntegerFinder {
+    findSmallestInt(args) {
+        return Math.min(...args);
+    }
+}
