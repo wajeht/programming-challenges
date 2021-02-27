b8f582cb1b88da646e649a70baec135b15475f24
Jaw <58354193+wajeht@users.noreply.github.com>
2021-02-27 03:29:33 -0600

    Update scramble.js

--- code-wars/scramblies/src/scramble.js
+++ code-wars/scramblies/src/scramble.js
@@ -11,6 +11,22 @@
 //     }
 //     return true;
 // }
+// ---------------------------------------------------------------------------------------------------
+// function scramble(str1, str2) {
+//     var hashtab = [...new Array(256)].map((x) => 0);
+
+//     str2.split('').forEach((ele) => hashtab[ele.charCodeAt(0)]++);
+//     str1.split('').forEach((ele) => hashtab[ele.charCodeAt(0)]--);
+
+//     hashtab = hashtab.filter((x) => x > 0);
+
+//     return hashtab.length == 0;
+// }
+// ---------------------------------------------------------------------------------------------------
+// function scramble(str1, str2) {
+//     let occurences = str1.split("").reduce((arr, cur) => { arr[cur] ? arr[cur]++ : arr[cur] = 1; return arr; }, {});
+//     return str2.split("").every((character) => --occurences[character] >= 0);
+//   }
 
 // NOTE: spent almost 2 days because my code was too slow.
 //       above is what i found on the internet.
